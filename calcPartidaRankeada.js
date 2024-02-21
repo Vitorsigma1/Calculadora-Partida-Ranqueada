8986f9c984632c8c8dd8d19f3f1439c083be9c5e
Vitorsigma1 <vitorbuono@yahoo.com.br>
2024-02-21 14:09:27 -0300

    thirt commit

--- calcPartidaRankeada.js
+++ calcPartidaRankeada.js
@@ -1,3 +1,37 @@
-let rank = "01"
-console.log(rank)
+
+
+let victories = 90
+let losses = 20
+const gameResult = ranked(victories, losses)
+
+function ranked(victories, losses){
+    let winningBalance = victories - losses
+    let level = " "
+
+        if (winningBalance <= 10){
+            level = "Iron"
+        }
+        else if (winningBalance >= 11 && winningBalance <= 20){
+            level = "Bronze"
+        }
+        else if (winningBalance >= 21 && winningBalance <= 50){
+            level = "Silver"
+        }
+        else if (winningBalance >= 51 && winningBalance <= 80){
+            level = "Gold"
+        }
+        else if (winningBalance >= 81 && winningBalance <= 90){
+            level = "Diamond"
+        }
+        else if (winningBalance >= 91 && winningBalance <= 100){
+            level = "Legendary"
+        }
+        else if (winningBalance >= 101){
+            level = "Immortal"
+        }
+            
+        return "The hero has a balance of " + winningBalance + " victories and is at level of " + level + "!"
+}
+console.log(gameResult)
+
 
